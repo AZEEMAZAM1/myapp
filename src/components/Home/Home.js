2b469506b8b22f56ef206ebae78ec7df9f214c99
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-04-27 11:35:19 +0100

    Update: added new features

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom"; // Import Link for navigation
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
@@ -13,30 +12,20 @@ function Home() {
         <Particle />
         <Container className="home-content">
           <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
-
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Muhammmad Azeem</strong>
-              </h1>
-
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-
-              {/* Add the News button here */}
-              <div style={{ marginTop: 20 }}>
-                <Link to="/news">
-                  <button className="btn btn-primary">Latest News</button>
-                </Link>
-              </div>
-            </Col>
+          <Col md={7} className="home-header">
+  {/* Replace the text with the transparent picture */}
+  <div style={{ textAlign: "center" }}>
+    <img
+      src={require("../../Assets/pic.png")} // Adjust the path to your image
+      alt="Transparent"
+      style={{
+        maxWidth: "100%",
+        height: "auto",
+        opacity: 0.8, // Set transparency
+      }}
+    />
+  </div>
+</Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,3 +1,60 @@
+// import React from "react";
+// import Particles from "react-tsparticles";
+
+// function Particle() {
+//   return (
+//     <Particles
+//       id="tsparticles"
+//       params={{
+//         particles: {
+//           number: {
+//             value: 160,
+//             density: {
+//               enable: true,
+//               value_area: 1500,
+//             },
+//           },
+//           line_linked: {
+//             enable: false,
+//             opacity: 0.03,
+//           },
+//           move: {
+//             direction: "right",
+//             speed: 0.05,
+//           },
+//           size: {
+//             value: 1,
+//           },
+//           opacity: {
+//             anim: {
+//               enable: true,
+//               speed: 1,
+//               opacity_min: 0.05,
+//             },
+//           },
+//         },
+//         interactivity: {
+//           events: {
+//             onclick: {
+//               enable: true,
+//               mode: "push",
+//             },
+//           },
+//           modes: {
+//             push: {
+//               particles_nb: 1,
+//             },
+//           },
+//         },
+//         retina_detect: true,
+//       }}
+//     />
+//   );
+// }
+
+// export default Particle;
+
+
 import React from "react";
 import Particles from "react-tsparticles";
 
@@ -8,45 +65,66 @@ function Particle() {
       params={{
         particles: {
           number: {
-            value: 160,
+            value: 80, // Adjust the number of diamonds
             density: {
               enable: true,
-              value_area: 1500,
+              value_area: 800,
             },
           },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
+          shape: {
+            type: "polygon", // Use polygon for diamond shape
+            polygon: {
+              sides: 4, // 4 sides to create a diamond shape
+            },
           },
           size: {
-            value: 1,
+            value: 6, // Adjust the size of the diamonds
+            random: true, // Randomize the size slightly
+          },
+          move: {
+            enable: true,
+            speed: 1, // Adjust the speed of movement
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
           },
           opacity: {
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.05,
-            },
+            value: 0.8, // Adjust the transparency of the diamonds
+            random: true,
+          },
+          color: {
+            value: "#5B7D9A", // Set the color of the diamonds (light navy blue)
           },
         },
         interactivity: {
           events: {
+            onhover: {
+              enable: true,
+              mode: "repulse", // Repulse effect on hover
+            },
             onclick: {
               enable: true,
-              mode: "push",
+              mode: "push", // Add more diamonds on click
             },
           },
           modes: {
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
             push: {
-              particles_nb: 1,
+              particles_nb: 4,
             },
           },
         },
         retina_detect: true,
+        background: {
+          color: {
+            value: "#001F3F", // Set background color to dark navy blue
+          },
+        },
       }}
     />
   );
